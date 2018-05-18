27fcb2d8e2b44c06981cfaebd99a7ddd41cfecac
captainuberawesome <darianovodon@gmail.com>
2018-05-18 19:20:10 +0700

    Add Credentials.swift example file

---
+++ Resources/Credentials_Example.swift
@@ -0,0 +1,13 @@
+//
+//  Credentials.swift
+//  Kitsune
+//
+//  Created by Daria Novodon on 29/04/2018.
+//
+
+import Foundation
+
+struct Credentials {
+  static let appClientID = "APP_CLIENT_ID"
+  static let appClientSecret = "APP_CLIENT_SECRET"
+}
